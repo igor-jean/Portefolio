d0613af3e6465151f9cd0a693eb5bd7a2cf9d938
Igor Jean <igorjean79@gmail.com>
2023-10-30 12:55:49 +0100

    ajout du cv

--- cv.php
+++ cv.php
@@ -1,4 +1,6 @@
 <?php require_once "insertionHtmlHeader.php" ?>
     <main>
+        <h2><a href="CV/CV_igor-jean.docx" tittle="Lien vers mon CV">Telecharger mon CV</a></h2> 
+        <img class="cv" src="img/CV_igor-jean.jpg" alt="Photo du CV">
     </main>
 <?php require_once "insertionHtmlHeader.php" ?>
